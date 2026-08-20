b49c381e796e471b13fb00e5267184c27cbda26b
Adrian Basuki <adriannugrohobasuki@gmail.com>
2026-08-20 12:00:05 +1000

    feat: show/hide password button

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -2,17 +2,19 @@ import BackButton from "@/components/BackButton";
 import { colours } from "@/constants/style";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "expo-router";
+import { Eye, EyeOff } from "lucide-react-native";
 import { useState } from "react";
 import {
   ActivityIndicator,
   KeyboardAvoidingView,
   Platform,
+  Pressable,
   ScrollView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-  View,
+  View
 } from "react-native";
 
 export default function AuthScreen() {
@@ -21,6 +23,7 @@ export default function AuthScreen() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordHidden, setHidden] = useState(true)
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
@@ -119,7 +122,7 @@ export default function AuthScreen() {
                 autoCapitalize="none"
                 autoCorrect={false}
                 placeholder="e.g. John"
-                placeholderTextColor={colours.text_secondary}
+                placeholderTextColor={colours.text_placeholder}
               />
             </>
             }
@@ -133,20 +136,30 @@ export default function AuthScreen() {
               autoCorrect={false}
               keyboardType="email-address"
               placeholder="e.g. name@example.com"
-              placeholderTextColor={colours.text_secondary}
+              placeholderTextColor={colours.text_placeholder}
             />
 
             <Text style={styles.label}>Password</Text>
-            <TextInput
-              style={styles.input}
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-              autoCapitalize="none"
-              autoCorrect={false}
-              placeholder="••••••••"
-              placeholderTextColor={colours.text_secondary}
-            />
+            <View>
+              <Pressable onPress={() => setHidden((prev) => !prev)} style={styles.showPassword}>
+                {
+                  passwordHidden ? 
+                  <EyeOff size={20} color={colours.text_secondary}/> :
+                  <Eye size={20} color={colours.text_secondary}/>
+                }
+              </Pressable>
+              <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={passwordHidden}
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder={passwordHidden ? "••••••••" : "YourPassword"}
+                placeholderTextColor={colours.text_placeholder}
+              />
+            </View>
+
           </View>
 
           {error ? <Text style={styles.error}>{error}</Text> : null}
@@ -220,10 +233,15 @@ const styles = StyleSheet.create({
     backgroundColor: colours.secondary_bg,
     borderRadius: 12,
     padding: 14,
-    fontSize: 14,
     color: colours.text_secondary,
     borderWidth: 1,
     borderColor: colours.border_1,
+  },
+  showPassword:{
+    position: "absolute",
+    right: 18,
+    top: 16,
+    zIndex: 50,
   },
   button: {
     backgroundColor: colours.accent_1,
